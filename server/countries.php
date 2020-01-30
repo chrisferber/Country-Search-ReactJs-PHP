4e99e09168ba7a2af44cec3e2e8475ef6734d949
Chris Ferber <chris@ferbers.us>
2020-01-30 13:13:44 -0600

    adapt backend for axios

--- server/countries.php
+++ server/countries.php
@@ -1,130 +1,305 @@
 <?php
-	// Main
-	session_start();
-	if(isset($_SESSION['countries'])) {
-		$countries = unserialize($_SESSION['countries']);
-	}
-	else {
-		$response = getCountriesResponse();
-		$formattedCountries = formatCountries($response);
 
-		$_SESSION['countries'] = serialize($formattedCountries);
-		$countries = $formattedCountries;
-	}
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Expose-Headers: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
 
-	$search = $_GET['countryInput'];
-	$results = getCountriesByInput($search, $countries);
 
-	echo $_GET['callback'] . '('.html_entity_decode(json_encode($results)).')';
-	// End Main
+// Thanks to Stephen Burgess for the orignal file, which has been modified per below.
+// https://github.com/stephenburgess8/countries/blob/master/src/countries.php
 
-	function getCountriesByInput($search, $allCountries) {
-		$countries = [];
-		$regions = [];
-		$subregions = [];
-		$count = 0;
+// Main
+// session_start();
 
-		foreach ($allCountries as $country) {
-			$countryMatchesSearchInput = false;
+// set to the user defined error handler
+// set_error_handler("customError",E_ALL);
 
-			$searchFields = [
-				strtolower($country['name']),
-				strtolower($country['alphaCode2']),
-				strtolower($country['alphaCode3'])
-			];
+$debug1 = TRUE;  // If set to TRUE we will print debug messages to the log
 
-			foreach ($searchFields as $countryProperty) {
-			     if (strpos($countryProperty, strtolower($search)) !== false) {
-			        $countryMatchesSearchInput = true;
-			    }
-			}
+$callapi = FALSE;  // If set to FALSE we will use SESSION info for API and not call API each time
 
-			if ($countryMatchesSearchInput) {
-				$count++;
-				array_push($countries, $country);
-				
-				$regionIsPresent = strlen($country['region']) > 0;
-				if($regionIsPresent && !in_array($country['region'], $regions)) {
-					array_push($regions, $country['region']);
-				}
-				
-				$subregionIsPresent = strlen($country['subregion']) > 0;
-				if($subregionIsPresent && !in_array($country['subregion'], $subregions)) {
-					array_push($subregions, $country['subregion']);
-				}
-			}
+    // Make sure the $_SESSION[countries] array is filled with the serializd list of all counties from previous call
 
-			if ($count == 50) {
-				break;
-			}
-		}
+    // to the restcountries.eu API, else call the API and fill $_SESSION['countries']
 
-		// Comment out these three lines to run in PHP lower than PHP 7
-		usort($countries, function($a, $b) {
-		    return $a['name'] <=> $b['name'];
-		});
+     if ($debug1 == TRUE) {
+        error_log("Entered countries.php, debug is set to ON", 0);
+         } else {
+        error_log("Entered countries.php, debug is set to OFF", 0);
+     }
 
-		sort($regions);
-		sort($subregions);
+     if ($callapi == TRUE) {
+        error_log("Entered countries.php, api call each time is set to YES", 0);
+        } else {
+        error_log("Entered countries.php, api call each time is set to NO", 0);
+     }
 
-		$results = array(
-			'search' => $search,
-			'countries' => $countries,
-			'regions' => $regions,
-			'subregions' => $subregions
-		);
+     error_log("Front end request method follows:", 0);
 
-		return $results;
-	}
+     error_log($_SERVER["REQUEST_METHOD"], 0);
 
-	function getCountriesResponse() {
-	    $curl = curl_init();
+     // Beginning of main loop. Either search a previusly filled ou contries array or get it and fill it up.
 
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://restcountries.eu/rest/v2/all",
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "GET",
-			CURLOPT_HTTPHEADER => array(
-			"cache-control: no-cache"
-			),
-		));
+    if (isset($_SESSION['countries']) and ($callapi == FALSE)) {
+        if ($debug1 == TRUE) {
+            error_log("countries.php: $SESSION isset is TRUE, $_SESSION countries sizeof() is:", 0);
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+            error_log(sizeof($_SESSION['countries'], 0)); // See https://www.php.net/manual/en/function.isset.php for isset 
 
-		curl_close($curl);
-		return json_decode($response, true);
-	}
+         }                                                         // See https://www.php.net/manual/en/function.session-start.php               
 
-	function formatCountries($response) {
-		$countries = [];
+                                                                   // See https://www.w3schools.com/php/php_sessions.asp for $_SESSION
 
-		foreach ($response as $country) {
-			$formattedCountry = array(
-				"name" => $country["name"],
-				"alphaCode2" => $country["alpha2Code"],
-				"alphaCode3" => $country["alpha3Code"],
-				"flag" => $country["flag"],
-				"population" => $country["population"],
-				"region" => $country["region"],
-				"subregion" => $country["subregion"]
-	        );
+        $countries = unserialize($_SESSION['countries']); 
 
-			$formattedCountry = array_map('htmlentities', $formattedCountry);
+                                                            // See https://www.w3resource.com/php/function-reference/unserialize.php
 
-			$languages = [];
-			foreach ($country["languages"] as $language) {
-				array_push($languages, $language["name"]);
-			}
-			$languages = array_map('htmlentities', $languages);
+                                                            // And great explanation of serialize / unserialize is here https://www.geeksforgeeks.org/serialize-deserialize-array-string/
 
-			$formattedCountry['languages'] = $languages;
+        } else {
 
-			array_push($countries, $formattedCountry);
-		}
+        $response = getCountriesResponse($debug1);                 // Sets variable response via getCountriesResponse() function below
 
-	    return $countries;
-	}
+        $formattedCountries = formatCountries($response);   // Sets variable via formatCountries() function below
+
+        $_SESSION['countries'] = serialize($formattedCountries); // sets session array variable with serialized formattedCountries variable
+
+        if ($debug1 == TRUE) {
+        error_log("countries.php: In getCountriesResponse and just set SESSION countries", 0);
+        }
+        $countries = $formattedCountries;                   // And finally, set countries variable for searching
+    }
+
+    // Now set $search to the search input that the front end passed us ($_GET gets input passed  by front end)
+
+    error_log("Input passed from front end:", 0);
+
+    $search = file_get_contents('php://input');
+
+    error_log($search, 0);
+
+    if ($debug1 == TRUE) {
+
+        error_log("countries.php: Input string we will search for:", 0);
+
+        error_log($search, 0);
+    }                               
+
+    // Fill up the requested countries into $results
+
+    // $search has the countries the front end told us to search for,
+
+    //     and $countries has an unserialized list of all countries from our call to the restcountries.eu API
+
+    $results = getCountriesByInput($search, $countries, $debug1);    // Call function below
+
+    if ($debug1 == TRUE) {
+        error_log("countries.php: Results of search", 0);
+
+        error_log(json_encode($results), 0);
+    } 
+
+    echo $_GET['callback'] . '('.html_entity_decode(json_encode($results)).')';  // Send sorted results back to calling front-end app
+
+    // End Main
+
+ 
+    // This function is called to search through the array of all countries and codes,
+
+    // with the front end app's country input, and if the country input matches a country as we move thrugh the array,
+
+    // that country (and its region and subregion) are pushed onto the $countries array
+
+    function getCountriesByInput($search, $allCountries, $debug1) {
+        $countries = [];                                    // Define arrays and a numeric variable
+
+        $regions = [];
+
+        $subregions = [];
+
+        $count = 0;
+
+        foreach ($allCountries as $country) {       // See https://stackoverflow.com/questions/5720260/as-operator-in-php
+            $countryMatchesSearchInput = false;     // Set boolean value
+
+            $searchFields = [                       // Define array of search elements. This array will hold country names and codes.
+
+                strtolower($country['name']),       // Country name in our array
+
+                strtolower($country['alphaCode2']),
+
+                strtolower($country['alphaCode3'])
+            ];
+
+            // For loop that searches for matches with country or country code that the user entered
+
+            foreach ($searchFields as $countryProperty) {                     
+                 if (strpos($countryProperty, strtolower($search)) !== false) {  // strpos finds the position of the first occurrence of a substring in a string
+
+                                                                                // strlower makes a string lowercase
+
+                    $countryMatchesSearchInput = true;                          // Set boolean to true if we get a match
+                }
+            }
+
+            if ($countryMatchesSearchInput) {                                    // We've found a match!
+                $count++;                                                       // Increment country match count
+
+                array_push($countries, $country);                               // array_push pushes one or more elements onto the end of array
+
+                $regionIsPresent = strlen($country['region']) > 0;              // strlen is just length of string
+
+                if($regionIsPresent && !in_array($country['region'], $regions)) { // Also push region onto array
+                    array_push($regions, $country['region']);  
+                }
+
+                $subregionIsPresent = strlen($country['subregion']) > 0;
+
+                if($subregionIsPresent && !in_array($country['subregion'], $subregions)) {
+                    array_push($subregions, $country['subregion']);
+                }
+            }
+
+            // The insructions said we should stop at 50 matches
+
+            if ($count == 50) {
+                break;
+            }
+        }
+
+        usort($countries, function($a, $b) {          // usort sorts an array by values using your user-defined comparison function
+            return $a['name'] <=> $b['name'];
+
+            // The <=> is the PHP "spaceship" operator
+
+            // Returns 0 if values on either side are equal
+
+            // Returns 1 if the value on the left is greater
+
+            // Returns -1 if the value on the right is greater                      
+        });
+
+        sort($regions);     // sort the array, lowest to highest
+
+        sort($subregions);  // sort the array, lowest to highest
+
+ 
+        $results = array(   // creates an array and populates the variable results with the array values
+            'search' => $search,
+
+            'countries' => $countries,
+
+            'regions' => $regions,
+
+            'subregions' => $subregions
+        );
+
+
+        if ($debug1 == TRUE) {
+
+        if ($count == 0) {
+            error_log("countries.php: Country search returned no results", 0);
+        } else {
+            error_log("countries.php: Country search found one or more countries", 0);
+            }
+         }
+
+        return $results;    // return list of matched countries and regions to caller
+    }
+
+ 
+
+    // This function gets all countries and country codes from the restcountries.eu API and puts all of them into $response
+
+    function getCountriesResponse($debug1) {
+         if ($debug1 == TRUE) {
+            error_log("countries.php: Entered getCountriesResponse", 0);
+         }
+
+        $curl = curl_init();
+
+        if ($debug1 == TRUE) {
+        if ($curl != 0) {
+            error_log("countries.php: Curl init success in getCountriesResponse", 0);
+        } else {
+            error_log("countries.php: Curl init error in getCountriesResponse", 0);
+            }
+        }
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://restcountries.eu/rest/v2/all",
+
+            CURLOPT_RETURNTRANSFER => true,
+
+            CURLOPT_TIMEOUT => 30,
+
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+
+            CURLOPT_CUSTOMREQUEST => "GET",
+
+            CURLOPT_HTTPHEADER => array(
+
+            "cache-control: no-cache"
+            ),
+        ));
+
+        $response = curl_exec($curl);   // Call the rescountries.eu API with curl
+
+        $err = curl_error($curl);      
+
+        curl_close($curl);
+
+        return json_decode($response, true);    // And return $response and "true" to caller.
+
+                                                // json_decode is a PHP function that takes a JSON encoded string and converts it into a PHP variable.
+    }
+
+    // This formats the $countries array with the country elements
+    function formatCountries($response) {
+        $countries = [];
+
+        foreach ($response as $country) {
+            $formattedCountry = array(
+
+                "name" => $country["name"],
+
+                "alphaCode2" => $country["alpha2Code"],
+
+                "alphaCode3" => $country["alpha3Code"],
+
+                "flag" => $country["flag"],
+
+                "population" => $country["population"],
+
+                "region" => $country["region"],
+
+                "subregion" => $country["subregion"]
+            );
+
+            $formattedCountry = array_map('htmlentities', $formattedCountry);
+
+            $languages = [];
+            foreach ($country["languages"] as $language) {
+                array_push($languages, $language["name"]);
+            }
+
+            $languages = array_map('htmlentities', $languages);
+
+            $formattedCountry['languages'] = $languages;
+
+            array_push($countries, $formattedCountry);
+        }
+
+        return $countries;
+    }
+
+    function customError($errno, $errstr, $errfile, $errline)  {
+        $abc = "Look - Error number $errno at line $errline error text is $errstr";
+
+        error_log($abc);
+
+        error_log("In error handler", 0);
+      }
+
 ?>
--- src/redux/sagas/countriesSaga.js
+++ src/redux/sagas/countriesSaga.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
+// import qs from "qs";
 
-// worker Saga: will be fired on 'FETCH_USER_EVENTS' actions
+
 function* fetchCountries(action) {
     try {
-        const searchResults = yield axios.post(`http://localhost:5000/server/countries.php`, action.payload.searchInput);
+        console.log('action.payload.searchInput=', action.payload.searchInput);
+        const searchResults = yield axios.post ('http://localhost:5000/server/countries.php', action.payload.searchInput, {
+            'Content-type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Origin': '*',
+        });
         yield console.log('searchResults for GET route in fetchCountries is', searchResults);
     } catch (error) {
         console.log('error in countriesSaga.js, fetchCountries request failed with error,', error);
     }
 }
-
-
 
 function* countriesSaga() {
     yield takeLatest('SEARCH_COUNTRIES', fetchCountries);
